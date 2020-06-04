5cea0837c57b64d76876c7153c628b4f63d1b723
l3 <rnjs5162@gmail.com>
2020-06-04 10:25:16 +0900

    button test commit

--- app/src/main/java/com/example/mes_app/MainActivity.java
+++ app/src/main/java/com/example/mes_app/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.mes_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn1 = (Button)findViewById(R.id.button);
 
-
+ btn1.setOnClickListener(new View.OnClickListener() {
+     @Override
+     public void onClick(View v) {
+         Toast.makeText(getApplicationContext(),"Login ",Toast.LENGTH_LONG).show();
+     }
+ });
     }
 }
