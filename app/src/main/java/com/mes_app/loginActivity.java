e58e99f71ffb1756a098529111cc844e21969f5d
l3 <rnjs5162@gmail.com>
2020-06-08 15:19:00 +0900

    layout

--- app/src/main/java/com/mes_app/loginActivity.java
+++ app/src/main/java/com/mes_app/loginActivity.java
@@ -2,7 +2,6 @@ package com.mes_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,16 +9,16 @@ import android.widget.Toast;
 
 import com.example.mes_app.R;
 
-public class MainActivity extends AppCompatActivity {
+public class loginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_loginMain);
         //dg
         //jg
 
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // 화면 가로로 고정
+
 
         Button btn1 = (Button)findViewById(R.id.button);
 
