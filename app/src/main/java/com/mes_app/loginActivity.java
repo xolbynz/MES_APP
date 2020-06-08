327b95ce059fbb93213ac4da1494dc06d48be64d
MUNSEJIN <anstpwls12@naver.com>
2020-06-08 16:50:09 +0900

    jtds 라이브러리 추가 Manifest 인터넷 연결 허용 추가 duild.gradle dependencies 수정

--- app/src/main/java/com/mes_app/loginActivity.java
+++ app/src/main/java/com/mes_app/loginActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.app.AlertDialog;
 
 import com.example.mes_app.R;
 
@@ -14,7 +15,7 @@ public class loginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loginMain);
+        setContentView(R.layout.activity_login);
         //dg
         //jg
 
