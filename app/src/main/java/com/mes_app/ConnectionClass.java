49e4fc62ae83375047548da249ec6e049a91c821
MUNSEJIN <anstpwls12@naver.com>
2020-06-12 14:42:02 +0900

    db 연동 성공

--- app/src/main/java/com/mes_app/ConnectionClass.java
+++ app/src/main/java/com/mes_app/ConnectionClass.java
@@ -10,41 +10,36 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionClass {
-    private Connection connection = null;
-    private String ConnectionURL = null;
-    private String errMsg = "";
+    public Connection connection = null;
+    public String ConnectionURL = null;
+    public String errMsg = "";
 
     @SuppressLint("NewApi")
-    public Connection getConnection(String user, String password, String database, String server)
-    {
+    public Connection getConnection(String user, String password, String database, String server) {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-        try
-        {
-            DriverManager.registerDriver((Driver)Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance());
-            ConnectionURL = "jdbc:jtds:sqlserver://" + server + "/" + database + ";user=" + user+ ";password=" + password + ";";
-            connection = DriverManager.getConnection(ConnectionURL);
+
+        try {
+            DriverManager.registerDriver((Driver) Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance());
+            connection = DriverManager.getConnection("jdbc:jtds:sqlserver://" + server ,user,password);
             Log.d("#DB", "after connection");
-        }
-        catch (SQLException se)
-        {
+        } catch (
+                SQLException se) {
             Log.e("error here 1 : ", se.getMessage());
             errMsg = se.getMessage();
-        }
-        catch (ClassNotFoundException e)
-        {
+        } catch (
+                ClassNotFoundException e) {
             Log.e("error here 2 : ", e.getMessage());
             errMsg = e.getMessage();
-        }
-        catch (Exception e)
-        {
+        } catch (
+                Exception e) {
             Log.e("error here 3 : ", e.getMessage());
             errMsg = e.getMessage();
         }
         return connection;
     }
 
-    public String getLastErrMsg(){
+    public String getLastErrMsg() {
         return errMsg;
     }
 }
