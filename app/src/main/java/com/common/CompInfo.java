4684bcc77b8e4065cd2957cb26949e328bb4d5f4
MUNSEJIN <anstpwls12@naver.com>
2020-06-15 15:35:16 +0900

    로그인 로직 변경

---
+++ app/src/main/java/com/common/CompInfo.java
@@ -0,0 +1,42 @@
+package com.common;
+
+public class CompInfo {
+    private String saupNo;
+    private String saupNm;
+    private String spCode;
+    private String spSite;
+
+    public String getSaupNo() {
+        return saupNo;
+    }
+
+    public String getSaupNm() {
+        return saupNm;
+    }
+
+    public String getSpCode() {
+        return spCode;
+    }
+
+    public String getSpSite() {
+        return spSite;
+    }
+
+    public void setSaupNo(String saupNo) {
+        this.saupNo = saupNo;
+    }
+
+    public void setSaupNm(String saupNm) {
+        this.saupNm = saupNm;
+    }
+
+    public void setSpCode(String spCode) {
+        this.spCode = spCode;
+    }
+
+    public void setSpSite(String spSite) {
+        this.spSite = spSite;
+    }
+
+
+}
