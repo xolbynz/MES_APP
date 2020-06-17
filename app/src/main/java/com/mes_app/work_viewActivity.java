4a196ddc02536ef52d0d394595f487220217e7a8
l3 <rnjs5162@gmail.com>
2020-06-17 16:47:08 +0900

    [ADD] work_viewActivity activity_work_view.xml

--- app/src/main/java/com/mes_app/work_viewActivity.java
+++ app/src/main/java/com/mes_app/work_viewActivity.java
@@ -1,9 +1,11 @@
 package com.mes_app;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,7 +13,7 @@ import com.example.mes_app.R;
 
 public class work_viewActivity  extends Fragment {
 
-
+    InputMethodManager imm; //키보드 내리기
     public  work_viewActivity() {
 
     }
@@ -19,6 +21,8 @@ public class work_viewActivity  extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        imm =(InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+
 
         return inflater.inflate(R.layout.activity_work_view, container, false);
     }
