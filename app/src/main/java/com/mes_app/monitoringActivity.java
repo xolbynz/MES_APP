84716c1c89a64e135e71bb05b56ed62343b76bb9
l3 <rnjs5162@gmail.com>
2020-06-22 16:57:18 +0900

    달력소환~

--- app/src/main/java/com/mes_app/monitoringActivity.java
+++ app/src/main/java/com/mes_app/monitoringActivity.java
@@ -1,9 +1,15 @@
 package com.mes_app;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -11,18 +17,68 @@ import androidx.fragment.app.Fragment;
 import com.example.mes_app.R;
 
 public class monitoringActivity  extends Fragment {
+    Context context;
+    ViewGroup rootView;
+    Button btn_startDate;
+    Button btn_endDate;
 
+    EditText edit_startDate;
+    EditText edit_endDate;
 
-    public monitoringActivity(){
+//    public monitoringActivity(){
+//
+//    }
 
+    public monitoringActivity(Context context){
+this.context=context;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        rootView = (ViewGroup) inflater.inflate(R.layout.activity_monitoring, container, false);
 
+        btn_startDate=rootView.findViewById(R.id.btn_start_moniter);
+        btn_endDate=rootView.findViewById(R.id.btn_end_moniter);
+        edit_endDate=rootView.findViewById(R.id.edit_end_moniter);
+        edit_startDate=rootView.findViewById(R.id.edit_start_moniter);
 
-        return  inflater.inflate(R.layout.activity_monitoring, container, false);
+        btn_startDate.setOnClickListener(showDate);
+        btn_endDate.setOnClickListener(showDate);
+        return rootView;
     }
+
+
+    View.OnClickListener showDate = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            DatePickerDialog dialog = new DatePickerDialog(context, listener, 2013, 10, 22);
+
+            dialog.show();
+
+
+
+
+
+        }
+    };
+
+    private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+            Toast.makeText(getContext(), year + "년" + monthOfYear + "월" + dayOfMonth +"일", Toast.LENGTH_SHORT).show();
+
+        }
+
+    };
+
+
+
+
+
 }
