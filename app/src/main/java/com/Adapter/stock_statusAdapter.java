924554465dba8e37b494feb6f4ad1e08573d84f5
l3 <rnjs5162@gmail.com>
2020-06-24 16:57:40 +0900

    = commit

--- app/src/main/java/com/Adapter/stock_statusAdapter.java
+++ app/src/main/java/com/Adapter/stock_statusAdapter.java
@@ -1,35 +1,30 @@
 package com.Adapter;
 
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
-import com.VO.RawVo;
+import com.VO.StockInputVo;
 import com.example.mes_app.R;
 
 import java.util.ArrayList;
 
-public class RawAdapter extends BaseAdapter {
+public class stock_statusAdapter extends BaseAdapter {
 
     Context context;
-    RawVo rawVo;
-    private ArrayList<RawVo> arrayList;
-
-    public void addItem(RawVo rawVo){
-        arrayList.add(rawVo);
-    }
+    StockInputVo stockInputVo;
+    private ArrayList<StockInputVo> arrayList;
 
     @Override
     public int getCount() {
-        return arrayList.size();
+        return 0;
     }
 
     @Override
     public Object getItem(int position) {
-        return arrayList.get(position);
+        return null;
     }
 
     @Override
@@ -40,14 +35,14 @@ public class RawAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+
         context = parent.getContext();
-        rawVo = arrayList.get(position);
+        stockInputVo = arrayList.get(position);
 
         if(convertView == null){
             LayoutInflater infaInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = infaInflater.inflate(R.layout.activity_stock_status, parent, false);
+            convertView = infaInflater.inflate(R.layout.activity_raw_view, parent, false);
         }
         return null;
     }
-
 }
