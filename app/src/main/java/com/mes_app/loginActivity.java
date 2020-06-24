f0853db437c56c682e753138ca3d0aa9ffde9d31
l3 <rnjs5162@gmail.com>
2020-06-24 10:28:09 +0900

    디자인커밋

--- app/src/main/java/com/mes_app/loginActivity.java
+++ app/src/main/java/com/mes_app/loginActivity.java
@@ -7,12 +7,10 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
